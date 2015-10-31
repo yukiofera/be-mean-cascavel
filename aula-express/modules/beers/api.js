e947cb7143440dcc836741cc115b4936e4eb3b2c
Caio Norder <caio@caionorder.com>
2015-10-31 19:16:59 -0200

    Clean in app.js, and replace all modules beer (lazy to fix, sorry.).

--- aula-express/modules/beers/api.js
+++ aula-express/modules/beers/api.js
@@ -2,27 +2,25 @@ var express = require('express');
 var router = express.Router();
 var Controller = require('./controller');
 
-console.log(router);
-
-/*GET users listing*/
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-    Controller.retrieve(req, res);
+  Controller.retrieve(req, res);
 });
 
 router.get('/:id', function(req, res, next) {
-    Controller.get(req, res);
+  Controller.get(req, res);
 });
 
 router.post('/', function(req, res, next) {
-    Controller.create(req, res);
+  Controller.create(req, res);
 });
 
 router.put('/:id', function(req, res, next) {
-    Controller.update(req, res);
+  Controller.update(req, res);
 });
 
 router.delete('/:id', function(req, res, next) {
-    Controller.delete(req, res);
+  Controller.delete(req, res);
 });
 
 module.exports = router;
--- aula-express/modules/beers/controller.js
+++ aula-express/modules/beers/controller.js
@@ -1,84 +1,85 @@
 var Model = require('./model');
 
-var msg = '';
-
 var Controller = {
-    create: function(req, res) {
-        var dados = req.body;
-
-        var model = new Model(dados);
-
-        model.save(function (err, data) {
-          if (err){
-            msg = 'Erro: ' + JSON.stringify(data);
-            return res.json(msg);
-        }
-
-        msg = 'pimba: '+JSON.stringify(data);
+  create: function(req, res){
+    var dados = req.body;
+    var model = new Model(dados);
+    model.save(function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        msg = 'Erro: ' + err;
         return res.json(msg);
-    });     
-    }
+      }
+      console.log('Cerveja Inserida: ', data);
+      msg = (data);
+      return res.json(msg);
+    });
+  }
+, retrieve: function(req, res){
+    var query = {};
 
-    , retrieve: function(req, res) {
-        var query = {};
+    Model.find(query, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        msg = 'Erro: ' + err;
+        return res.json(msg);
+      }
+      console.log('Listagem: ', data);
+      msg = (data);
+      return res.json(msg);
+    });
+  }
+, get: function(req, res){
+    var query = {_id: req.params.id};
 
-        Model.find(query, function (err, data) {
-            if (err){
-                msg = 'Erro: ' + JSON.stringify(data);
-                return res.json(msg);
-            }
+    Model.findOne(query, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        msg = 'Erro: ' + err;
+        return res.json(msg);
+      }
+      console.log('Listagem: ', data);
+      msg = (data);
+      return res.json(msg);
+    });
+  }
+, update: function(req, res){
+    var query = {_id: req.params.id};
 
-            msg = 'pimba: '+JSON.stringify(data);
-            return res.json(msg);
-        });
-    }
-    
-    , get: function(req, res) {
-        var query = {_id: req.params.id};
+    var mod = req.body;
 
-        Model.findOne(query, function (err, data) {
-            if (err){
-                msg = 'Erro: ' + JSON.stringify(data);
-                return res.json(msg);
-            }
+    var optional = {
+      upsert: false,
+      multi: true
+    };
 
-            msg = 'pimba: '+JSON.stringify(data);
-            return res.json(msg);
-        });
-    }
-    
-    , update: function(req, res) {
-        var query = {_id: req.params.id};
-        var  mod = {
-          name: 'Budweiser'
-          , alcohol: 4
-          , price: 6
-      };
-
-      Model.update(query, mod, function (err, data) {
-        if (err){
-            msg = 'Erro: ' + JSON.stringify(data);
-            return res.json(msg);
-        }
-
-        res.json('Cervejas atualizadas com sucesso: ' + JSON.stringify(data));      
+    Model.update(query, mod, function (err, data) {
+      if (err){
+        console.log('Erro: ', err);
+        msg = 'Erro: ' + err;
+        return res.json(msg);
+      }
+      console.log('Cerveja alteradas: ', data);
+      msg = (data);
+      return res.json(msg);
     });
 
   }
-  
-  , delete: function(req, res) {
+, delete: function(req, res){
     var query = {_id: req.params.id};
 
-    Model.remove(query, function (err, data) {
+    Model.remove(query, function(err, data) {
       if (err){
         console.log('Erro: ', err);
-    }
-    else{
-        console.log('Cerveja deletada com sucesso', data.result);    
-    }
-});
+        msg = 'Erro: ' + err;
+        return res.json(msg);
+      }
+      console.log('Cerveja(s) atualizada(s): ', data);
+      msg = (data);
+      return res.json(msg);
+    });
 
+  }
 }
-};
 
 module.exports = Controller;
