9586fe410d1dc12d177b5e776a9045b5c92cc010
yukiofera <yukiofera@gmail.com>
2015-10-31 18:40:48 -0200

    initial commit

---
+++ aula-express/modules/beers/api.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var Controller = require('./controller');
+
+console.log(router);
+
+/*GET users listing*/
+router.get('/', function(req, res, next) {
+    Controller.retrieve(req, res);
+});
+
+router.get('/:id', function(req, res, next) {
+    Controller.get(req, res);
+});
+
+router.post('/', function(req, res, next) {
+    Controller.create(req, res);
+});
+
+router.put('/:id', function(req, res, next) {
+    Controller.update(req, res);
+});
+
+router.delete('/:id', function(req, res, next) {
+    Controller.delete(req, res);
+});
+
+module.exports = router;
